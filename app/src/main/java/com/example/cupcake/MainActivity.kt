1e5c274871d0eb113ffb50cbf6a35ca0be8b9cd7
Umar Saidu Auna <usaidu@gidimobile.com>
2021-05-03 09:18:27 +0100

    feat: 🎸 3. Complete the Navigation Graph

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -15,9 +15,25 @@
  */
 package com.example.cupcake
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
  * Activity for cupcake order flow.
  */
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+// https://developer.android.com/courses/pathways/android-basics-kotlin-unit-3-pathway-4
+// https://developer.android.com/codelabs/basic-android-kotlin-training-shared-viewmodel
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // This will do the following: Show a title in the app bar based off of the destination's label, 
+        // and display the Up button whenever you're not on a top-level destination.
+        setupActionBarWithNavController(navController)
+    }
+}
